9b8e2405b835bd329341f08fc288d903e0781d00
random83 <philip83.santos@gmail.com>
2019-04-26 16:07:19 -0400

    more basics

--- java/com/random83/AnAttempt/util/Reference.java
+++ java/com/random83/AnAttempt/util/Reference.java
@@ -5,6 +5,6 @@ public class Reference {
 	public static final String NAME = "AnAttempt";
 	public static final String VERSION = "0.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY = "";
-	public final static String COMMON_PROXY = "";
+	public static final String CLIENT_PROXY = "com.random83.AnAttempt.proxy.ClientProxy";
+	public final static String COMMON_PROXY = "com.random83.AnAttempt.proxy.CommonProxy";
 }
