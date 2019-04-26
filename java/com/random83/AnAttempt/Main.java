b8c4dcee0ddd7f8ad47fc8967903980c44e8e471
random83 <philip83.santos@gmail.com>
2019-04-26 15:46:20 -0400

    Initial commit

---
+++ java/com/random83/AnAttempt/Main.java
@@ -0,0 +1,10 @@
+package com.random83.AnAttempt;
+
+import com.random83.AnAttempt.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+
+}
